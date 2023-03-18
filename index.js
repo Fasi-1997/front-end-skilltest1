a8dc62880745e389e5805a6bfbfc78ef08212c85
fasi-1997 <mdfacis@gmail.com>
2023-03-18 15:36:18 +0530

    Added comments and minor enhancements

--- index.js
+++ index.js
@@ -5,11 +5,14 @@ const addTaskInput = document.getElementById('add');
 const tasksCounter = document.getElementById('tasks-counter');
 const addTaskIcon = document.getElementById('add-icon');
 
+//This metod masrks all the task as completed
 function completeAllTasks() {
     for (i = 0; i < tasks.length; i++) {
         tasks[i].IsCompleted = true;
     }
     renderList(tasks);
+    clearFilter();
+    showNotification('Marked all task as complete.');
 }
 
 function addTaskToUI(task) {
@@ -29,12 +32,14 @@ function renderList(todolist) {
     tasksCounter.innerHTML = todolist.length;
 }
 
+//Toggles IsComplete flage for a given task id
 function toggletask(taskId) {
     const taskIndex = tasks.findIndex((task => task.id == taskId));
     tasks[taskIndex].IsCompleted = !tasks[taskIndex].IsCompleted;
     renderList(tasks);
     clearFilter();
 }
+//This method filters tasks; filter options are All, incomplete and complete 
 function filterTasks(filter) {
     const newTasks = tasks.filter(function (task) {
         if (filter == 'completed')
@@ -47,16 +52,18 @@ function filterTasks(filter) {
     renderList(filteredTasks);
     setActiveButton();
 }
-
+//This metod deletes the task for given task id
 function deleteTask(taskId) {
     const newTasks = tasks.filter(function (task) {
         return task.id !== taskId;
     })
     tasks = newTasks;
     renderList(tasks);
+    clearFilter();
     showNotification('Task deleted.');
 }
 
+// This method marks the completed task as incomplete
 function clearCompletedTasks() {
     const completedTasks = tasks.filter(function (task) {
         return task.IsCompleted == true;
@@ -64,16 +71,17 @@ function clearCompletedTasks() {
     if (completedTasks.length == 0) {
         showNotification('There are no completed tasks.');
         return;
-    }    
-    
-    for(i=0;i<completedTasks.length;i++){
+    }
+
+    for (i = 0; i < completedTasks.length; i++) {
         const taskIndex = tasks.findIndex((task => task.id == completedTasks[i].id));
         tasks[taskIndex].IsCompleted = false;
     }
     renderList(tasks);
+    clearFilter();
     showNotification('Cleared completed tasks.');
 }
-
+// this method adds task 
 function addTask() {
     const title = addTaskInput.value;
     if (!title) {
@@ -97,7 +105,7 @@ function addTask() {
 function showNotification(message) {
     alert(message);
 }
-
+//This method is used to toggle the add taskk icon; icon will show only if title has some text
 function toggleButton() {
     if (addTaskInput.value === "") {
         addTaskIcon.style.display = "none";
@@ -105,7 +113,7 @@ function toggleButton() {
         addTaskIcon.style.display = "block";
     }
 }
-
+//Event delegation for delete and toggle IsCompleted  
 function handleClick(e) {
     const target = e.target;
     if (target.className === 'delete') {
@@ -116,18 +124,16 @@ function handleClick(e) {
         const taskId = target.id;
         toggletask(taskId);
         return;
-    } else if (target.className === 'complete-tasks') {
-        completeAllTasks();
-        renderList(tasks);
-        return;
     }
 }
-function clearFilter(){
+// This method clears the filter; this method is called whenever a change happens to task list
+function clearFilter() {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
     var allFilter = document.getElementById("all");
     allFilter.className += " active";
 }
+//This method will be used to set active button when a filter is selected
 function setActiveButton() {
     var btnContainer = document.getElementById("btn-group");
     var btns = btnContainer.getElementsByClassName("btn");
